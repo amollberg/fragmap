908f564e4635dc650ce5ed9d6b6d8b9ec5e54633
amollberg <amollberg@users.noreply.github.com>
2016-07-04 22:12:29 +0200

    improved git show arguments, added generate-matrix.py

--- list-hunks.sh
+++ list-hunks.sh
@@ -1,7 +1,12 @@
 #!/bin/env bash
 
 while read -r rev; do
-	echo $rev 
+    echo $rev
     #git diff "$rev~1" "$rev"  --
-	git show "$rev"  -- | grep "^@"
+    git show -U0 --no-color "$rev"  -- # | grep "^[@+-]"
 done < <(git rev-list "$1")
+
+# TODO: Integrate with interactive rebase, edit buffer or something
+#git rebase -i "$1"
+#cat .git/rebase-merge/git-rebase-todo
+
