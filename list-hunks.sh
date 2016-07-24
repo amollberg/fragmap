53e5519ca09ab2a97eff1e929521852631265b4b
amollberg <amollberg@users.noreply.github.com>
2016-07-24 16:25:53 +0200

    Fixed: list-hunks.sh now lists in chronological order (ascending)

--- list-hunks.sh
+++ list-hunks.sh
@@ -4,9 +4,8 @@ while read -r rev; do
     echo $rev
     #git diff "$rev~1" "$rev"  --
     git show -U0 --no-color "$rev"  -- # | grep "^[@+-]"
-done < <(git rev-list "$1")
+done < <(git rev-list --reverse "$1")
 
 # TODO: Integrate with interactive rebase, edit buffer or something
 #git rebase -i "$1"
 #cat .git/rebase-merge/git-rebase-todo
-
