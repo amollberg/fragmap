877d3aa1ce8619bba2bc8cbe1000a2a4eed337fe
amollberg <amollberg@users.noreply.github.com>
2016-07-24 11:00:09 +0200

    Added console_ui with mockup

---
+++ console_ui.py
@@ -0,0 +1,34 @@
+from parse_patch import *
+from generate_matrix import *
+
+CONSOLE_WIDTH = 80
+
+# TODO: Change name?
+def print_hunkogram(diff_list):
+  matrix = generate_matrix(diff_list)
+  matrix_width = len(matrix[0])
+  hash_width = 8
+  padded_matrix_width = max(CONSOLE_WIDTH/2, matrix_width)
+  max_commit_width = min(CONSOLE_WIDTH/2, CONSOLE_WIDTH - (hash_width + 1 + 1 + padded_matrix_width))
+  for row in matrix:
+    commit_msg = "Test"
+    hash = "abcd0123"
+    # Pad short commit messages
+    commit_msg = commit_msg.ljust(max_commit_width, ' ')
+    # Truncate long commit messages
+    commit_msg = commit_msg[0:min(max_commit_width,len(commit_msg))]
+    # Print hash, commit, matrix row
+    hash = hash[0:hash_width]
+    print hash, commit_msg, ''.join(row)
+
+def main():
+  pp = PatchParser()
+  lines = [line.rstrip() for line in sys.stdin]
+  print lines
+  diff_list = pp.parse(lines)
+  print_hunkogram(diff_list)
+
+
+
+if __name__ == '__main__':
+  main()
