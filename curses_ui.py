340e470e72e5c1f9389c3d2df5ed8ab9bfa71ad9
amollberg <amollberg@users.noreply.github.com>
2016-08-03 19:23:17 +0200

    Error or too wide matrix, Added DEBUG_CURSES

--- curses_ui.py
+++ curses_ui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+DEBUG_CURSES=False
 
 import npyscreen
 class HunkogramApp(npyscreen.NPSApp):
@@ -12,7 +13,6 @@ class HunkogramApp(npyscreen.NPSApp):
     def main(self):
         # These lines create the form and populate it with widgets.
         # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F = npyscreen.ActionFormWithMenus(name = "Welcome to Npyscreen",)
         #matrix = [['#','.','#','#','#','.','.'],
         #          ['#','#','.','.','.','#','.'],
         #          ['.','#','.','#','.','.','.']]
@@ -23,13 +23,24 @@ class HunkogramApp(npyscreen.NPSApp):
         n_rows = len(matrix)
         hash_width = self._hash_width
         msg_width = self._console_width - (2 + hash_width + 1 + 2*n_matrix_cols + 2)
+        if msg_width < 1:
+            F = npyscreen.ActionFormWithMenus(name = "Error: Diagram too wide")
+            F.edit()
+            return
+        if DEBUG_CURSES:
+            print matrix, diff_list, n_matrix_cols, n_cols, n_rows, hash_width, msg_width
         grid = [[''] * n_cols ]* n_rows
+
         for r in range(n_rows):
             #hash = 'abcd0123'
             hash = diff_list._patches[r]._header._hash[0:hash_width]
             #commit_msg = 'Test commit message about this or that'
             commit_msg = diff_list._patches[r]._header._message[0] # First row of message
+            grid_column_widths = [hash_width, msg_width] + [2]*len(matrix[0])
+            if DEBUG_CURSES:
+                print hash, commit_msg, grid_column_widths
             grid[r] = [hash, commit_msg] + matrix[r]
+        F = npyscreen.ActionFormWithMenus(name = "Welcome to Npyscreen",)
         g = F.add(npyscreen.SimpleGrid, values=grid, name="simple grid",
-                  column_width=[hash_width, msg_width] + [2]*len(matrix[0]), col_margin=0)
+                  column_width=grid_column_widths, col_margin=0)
         F.edit()
