39df3725b556f054653cbcb4c6395981797fdd5d
amollberg <amollberg@users.noreply.github.com>
2016-10-06 21:29:13 +0200

    Created Hunkogram class and used in generate_matrix.py

--- generate_matrix.py
+++ generate_matrix.py
@@ -44,77 +44,84 @@ def print_node_line_relation_table(node_lines):
     print ''.join(row)
 
 
-# Group node lines that are equal, i.e. that at the first
-# common diff are at the same position and of the same kind.
-# As a note, at any subsequent diffs they will consequently be the same too.
-def group_fragment_bound_lines(node_lines):
-  """
-  Takes a list of up-to date list of bound node lines.
-  Returns a list with ordered bound node lines
-  grouped by position (file, line) at first common diff.
-  """
-  node_lines = sorted(node_lines)
-  debug.log(debug.sorting, "Sorted lines:", node_lines)
-  groups = []
-  for node_line in node_lines:
-    added = False
-    for group in groups:
-      inter_diff_collision = False
-      for member in group:
-        if member._startdiff_i == node_line._startdiff_i \
-        and member._kind != node_line._kind:
-          inter_diff_collision = True
+
+
+class Hunkogram():
+
+  def __init__(self, ast):
+    self.ast = ast
+    debug.log(debug.matrix, "AST:", ast)
+    self.node_lines = update_all_positions_to_latest(ast._patches)
+
+  # Group node lines that are equal, i.e. that at the first
+  # common diff are at the same position and of the same kind.
+  # As a note, at any subsequent diffs they will consequently be the same too.
+  def group_fragment_bound_lines(self):
+    """
+    Takes a list of up-to date list of bound node lines.
+    Returns a list with ordered bound node lines
+    grouped by position (file, line) at first common diff.
+    """
+    self.node_lines = sorted(self.node_lines)
+    debug.log(debug.sorting, "Sorted lines:", self.node_lines)
+    groups = []
+    for node_line in self.node_lines:
+      added = False
+      for group in groups:
+        inter_diff_collision = False
+        for member in group:
+          if member._startdiff_i == node_line._startdiff_i \
+          and member._kind != node_line._kind:
+            inter_diff_collision = True
+            break
+        if node_line == group[0] and not inter_diff_collision:
+          # Append to group
+          group += [node_line]
+          added = True
           break
-      if node_line == group[0] and not inter_diff_collision:
-        # Append to group
-        group += [node_line]
-        added = True
-        break
-    if not added:
-      # Create new group
-      groups += [[node_line]]
-  return groups
+      if not added:
+        # Create new group
+        groups += [[node_line]]
+    return groups
 
+  # Iterate over the list, placing markers at column i row j if i >= a start node of revision j and i < end node of same revision
+  def generate_matrix(self):
+    if debug.is_logging(debug.grouping):
+      print_node_line_relation_table(self.node_lines)
+    debug.log(debug.matrix, "Node lines:", self.node_lines)
 
-# Iterate over the list, placing markers at column i row j if i >= a start node of revision j and i < end node of same revision
+    grouped_node_lines = self.group_fragment_bound_lines()
+    debug.log(debug.matrix, "Grouped lines:", grouped_node_lines)
 
+    n_rows = len(self.ast._patches) # TODO: Make this dependent on self.node_lines instead
+    n_cols = len(grouped_node_lines)
+    debug.log(debug.grid, "Matrix size: rows, cols: ", n_rows, n_cols)
+    matrix = [['.' for i in xrange(n_cols)] for j in xrange(n_rows)]
+    for r in range(n_rows):
+      diff_i = r
+      inside_fragment = False
+      for c in range(n_cols):
+        node_line_group = grouped_node_lines[c]
+        debug.log(debug.grid, "%d,%d: %s" %(r, c, node_line_group))
+        if True: #earliest_diff(node_line_group) <= diff_i:
+          for node_line in node_line_group:
+            # If node belongs in on this row
+            if node_line._startdiff_i == diff_i:
+              inside_fragment = (node_line._kind == FragmentBoundNode.START)
+              debug.log(debug.grid, "Setting inside_fragment =", inside_fragment)
+              # If it was updated to False:
+              if not inside_fragment:
+                # False overrides True so that if start and end from same diff
+                # appear in same group we don't get stuck at True
+                break
+          debug.log(debug.grid, "%d,%d: %d" %(r, c, inside_fragment))
+          if inside_fragment:
+            matrix[r][c] = '#'
+    return matrix
 
-def generate_matrix(ast):
-  debug.log(debug.matrix, "AST:", ast)
-  node_lines = update_all_positions_to_latest(ast._patches)
-  if debug.is_logging(debug.grouping):
-    print_node_line_relation_table(node_lines)
-  debug.log(debug.matrix, "Node lines:", node_lines)
-
-  grouped_node_lines = group_fragment_bound_lines(node_lines)
-  debug.log(debug.matrix, "Grouped lines:", grouped_node_lines)
-  #bound_list = generate_fragment_bound_list(ast)
-
-  n_rows = len(ast._patches)
-  n_cols = len(grouped_node_lines)
-  debug.log(debug.grid, "Matrix size: rows, cols: ", n_rows, n_cols)
-  matrix = [['.' for i in xrange(n_cols)] for j in xrange(n_rows)]
-  for r in range(n_rows):
-    diff_i = r
-    inside_fragment = False
-    for c in range(n_cols):
-      node_line_group = grouped_node_lines[c]
-      debug.log(debug.grid, "%d,%d: %s" %(r, c, node_line_group))
-      if True: #earliest_diff(node_line_group) <= diff_i:
-        for node_line in node_line_group:
-          # If node belongs in on this row
-          if node_line._startdiff_i == diff_i:
-            inside_fragment = (node_line._kind == FragmentBoundNode.START)
-            debug.log(debug.grid, "Setting inside_fragment =", inside_fragment)
-            # If it was updated to False:
-            if not inside_fragment:
-              # False overrides True so that if start and end from same diff
-              # appear in same group we don't get stuck at True
-              break
-        debug.log(debug.grid, "%d,%d: %d" %(r, c, inside_fragment))
-        if inside_fragment:
-          matrix[r][c] = '#'
-  return matrix, grouped_node_lines
+  def str(self):
+    matrix = self.generate_matrix()
+    return '\n'.join([''.join(row) for row in matrix])
 
 
 def main():
@@ -122,9 +129,8 @@ def main():
   lines = [line.rstrip() for line in sys.stdin]
   diff_list =  pp.parse(lines)
   debug.log(debug.matrix, diff_list)
-  matrix, _ = generate_matrix(diff_list)
-  for row in matrix:
-    print ''.join(row)
+  h = Hunkogram(diff_list)
+  print h.str()
 
 
 if __name__ == '__main__':
