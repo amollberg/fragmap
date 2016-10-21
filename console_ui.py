555cc09ebf152e1d8900d586d6e3a41058764bc4
amollberg <amollberg@users.noreply.github.com>
2016-10-21 21:12:06 +0200

    Factored out make_hunkogram, added --brief arg

--- console_ui.py
+++ console_ui.py
@@ -16,16 +16,21 @@ except ImportError:
 
 CONSOLE_WIDTH = 80
 
+def make_hunkogram(diff_list, brief=False):
+  hunkogram = Hunkogram.from_ast(diff_list)
+  if brief:
+    hunkogram = hunkogram.group_by_patch_connection()
+  return hunkogram
+
 # TODO: Change name?
-def print_hunkogram(diff_list):
-  hunkogram = Hunkogram.from_ast(diff_list).group_by_patch_connection()
+def print_hunkogram(hunkogram):
   matrix = hunkogram.generate_matrix()
   matrix_width = len(matrix[0])
   hash_width = 8
   padded_matrix_width = min(CONSOLE_WIDTH/2, matrix_width)
   max_commit_width = min(CONSOLE_WIDTH/2, CONSOLE_WIDTH - (hash_width + 1 + 1 + padded_matrix_width))
   for r in range(len(matrix)):
-    cur_patch = diff_list._patches[r]._header
+    cur_patch = hunkogram.patches[r]._header
     commit_msg = cur_patch._message[0] # First row of message
     hash = cur_patch._hash
     # Pad short commit messages
@@ -36,11 +41,9 @@ def print_hunkogram(diff_list):
     hash = hash[0:hash_width]
     print hash, commit_msg, ''.join(matrix[r])
 
-def display_hunkogram_screen(diff_list):
-  hunkogram = Hunkogram.from_ast(diff_list).group_by_patch_connection()
+def display_hunkogram_screen(hunkogram):
   hash_width = 8
   App = HunkogramApp()
-  App._diff_list = diff_list
   App._hunkogram = hunkogram
   App._console_width = CONSOLE_WIDTH
   App._hash_width = hash_width
@@ -50,6 +53,7 @@ def main(parent_argparser):
   # Parse command line arguments
   argparser = argparse.ArgumentParser(parents=[parent_argparser])
   argparser.add_argument('-p', '--plain', action='store_true', required=False)
+  argparser.add_argument('-b', '--brief', action='store_true', required=False)
   args, unknown_args = argparser.parse_known_args()
   # Parse diffs
   pp = PatchParser()
@@ -57,10 +61,11 @@ def main(parent_argparser):
   debug.log(debug.console, lines)
   diff_list = pp.parse(lines)
   debug.log(debug.console, diff_list)
+  hunkogram = make_hunkogram(diff_list, args.brief)
   if not args.plain and NPYSCREEN_AVAILABLE:
-    display_hunkogram_screen(diff_list)
+    display_hunkogram_screen(hunkogram)
   else:
-    print_hunkogram(diff_list)
+    print_hunkogram(hunkogram)
 
 if __name__ == '__main__':
   debug_parser = debug.parse_args(extendable=True)
