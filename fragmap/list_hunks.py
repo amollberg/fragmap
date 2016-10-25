9b562416048bf458119c3938dc509f4a10fd9e77
amollberg <amollberg@users.noreply.github.com>
2016-10-25 21:33:58 +0200

    Performance improvement: Call 'git show' once on all revs together

--- fragmap/list_hunks.py
+++ fragmap/list_hunks.py
@@ -24,9 +24,9 @@ def get_diff(rev_range_str):
   output = []
   try:
     output += get_output_lines([GIT, 'diff', '-U0', '--no-color'])
-    for rev in get_output_lines([GIT, 'rev-list', '--reverse', rev_range_str]):
-      if rev != '':
-        output += get_output_lines([GIT, 'show', '-U0', '--no-color', rev])
+    rev_list = get_output_lines([GIT, 'rev-list', '--reverse', rev_range_str])
+    if rev_list:
+      output += get_output_lines([GIT, 'show', '-U0', '--no-color'] + rev_list)
     return output
   except subprocess.CalledProcessError, e:
     if e.returncode == 129:
