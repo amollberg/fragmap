6ffec55a7f1fc108c75542b854f1e313d31f06c5
amollberg <amollberg@users.noreply.github.com>
2018-10-01 22:48:10 +0200

    Web: Nicer styling

--- fragmap/web_ui.py
+++ fragmap/web_ui.py
@@ -12,22 +12,56 @@ def open_fragmap_page(fragmap):
   print(matrix)
 
   def render_cell(cell):
-    with tag('span', **{'class': 'matrix_cell'}):
-      if cell.kind == Cell.CHANGE:
-        text('#')
-      if cell.kind == Cell.BETWEEN_CHANGES:
-        text('^')
-      if cell.kind == Cell.NO_CHANGE:
-        text('.')
+    classes = ['matrix_cell']
+    if cell.kind == Cell.CHANGE:
+      classes.append('cell_change')
+      cell_text = '#'
+    elif cell.kind == Cell.BETWEEN_CHANGES:
+      classes.append('cell_between_changes')
+      cell_text = '^'
+    elif cell.kind == Cell.NO_CHANGE:
+      classes.append('cell_no_change')
+      cell_text = '.'
+    with tag('div', **{'class': ' '.join(classes)}):
+      text(cell_text)
+
 
   doc, tag, text = Doc().tagtext()
   def get_html():
     with tag('html'):
       with tag('head'):
         with tag('style', type='text/css'):
-          text('th { text-align: left; }')
-          text('.commit_hash { font-family: monospace; }')
-          text('.matrix_cell { font-family: monospace; }')
+          text("""
+               table {
+                 border-collapse: collapse;
+               }
+               th {
+                 text-align: left;
+                 vertical-align: bottom;
+                 padding: 0;
+               }
+               .commit_hash {
+                 font-family: monospace;
+                 margin-right: 10pt;
+               }
+               .commit_message {
+                 margin-right: 10pt;
+               }
+               .matrix_cell, .matrix_cell > div {
+                 font-family: monospace;
+                 width: 100%;
+                 height: 100%;
+                 padding: 0;
+               }
+               .cell_change {
+                 background-color: yellow;
+               }
+               .cell_between_changes {
+                 background-color: red;
+               }
+               .cell_no_change {
+                 background-color: green;
+               }""")
       with tag('body'):
         with tag('table'):
           for r in range(len(matrix)):
@@ -36,10 +70,11 @@ def open_fragmap_page(fragmap):
             hash = cur_patch._hash
             with tag('tr'):
               with tag('th'):
-                text(commit_msg)
-              with tag('th'):
                 with tag('span', **{'class': 'commit_hash'}):
                   text(hash[0:8])
+              with tag('th'):
+                with tag('span', **{'class': 'commit_message'}):
+                  text(commit_msg)
               for c in matrix[r]:
                 with tag('th'):
                   render_cell(c)
