91c7494435ae23bf071791233a23dcfbc6efd00c
amollberg <amollberg@users.noreply.github.com>
2018-11-13 21:14:24 +0100

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "fragmap",
-    version = "0.2",
+    version = "0.2.1",
     author = "Alexander Mollberg",
     author_email = "amollberg@users.noreply.github.com",
     description = ("Visualize a timeline of Git commit changes on a grid"),
