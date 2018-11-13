8143847c6dfcd886248fb6ff14105f2cdbf8afe3
amollberg <amollberg@users.noreply.github.com>
2018-11-13 20:13:02 +0100

    Fix: Add forgotten file console_color

---
+++ fragmap/console_color.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+ANSI_ESC = '\033'
+ANSI_BG_RED = ANSI_ESC + '[41m'
+ANSI_BG_WHITE = ANSI_ESC + '[47m'
+ANSI_BG_MAGENTA = ANSI_ESC + '[45m'
+ANSI_FG_RED = ANSI_ESC + '[31m'
+ANSI_FG_CYAN = ANSI_ESC + '[36m'
+ANSI_FG_BLUE = ANSI_ESC + '[34m'
+ANSI_RESET = ANSI_ESC + '[0m'
