0c0b606cfa2820353bde985a1765969f15f6d558
amollberg <amollberg@users.noreply.github.com>
2019-05-11 11:45:06 +0200

    More specific imports in general

--- tests/general.py
+++ tests/general.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 from common import *
-from load_commits import *
-from generate_matrix import *
+from load_commits import Range
+from update_fragments import update_inherited_bound, FragmentBoundNode
+from generate_matrix import Cell
 import list_hunks
 import debug
 
