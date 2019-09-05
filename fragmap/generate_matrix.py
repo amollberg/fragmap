903bc5e97a4f77faae8d9183827fd950381d6d20
amollberg <amollberg@users.noreply.github.com>
2019-09-05 21:17:17 +0200

    Use pipe as BETWEEN marker

--- fragmap/generate_matrix.py
+++ fragmap/generate_matrix.py
@@ -286,7 +286,7 @@ class Fragmap:
           # Make background red
           return ANSI_BG_RED + ' ' + ANSI_RESET
         else:
-          return '.'
+          return '|'
       if cell.kind == CellKind.NO_CHANGE:
         return '.'
       assert False, "Unexpected cell kind: %s" % (cell.kind)
